4adcdf837da63ec45596b31710d663b03c03d1d4
Muhammad Mousa Khalid <mkmousa76@gmail.com>
2023-02-11 13:43:19 +0500

    first commit

---
+++ src/utils/index.js
@@ -0,0 +1,29 @@
+// import { surpriseMePrompts } from "../constants";
+// import FileSaver from "file-saver";
+
+// export function getRandomPrompt(prompt) {
+//   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+//   const randomPrompt = surpriseMePrompts[randomIndex];
+//   if (randomPrompt === prompt) return getRandomPrompt(prompt);
+//   return randomPrompt;
+// }
+
+// export async function downloadImage(_id, photo) {
+//   FileSaver.saveAs(photo, `download-${_id}.jpg`);
+//   console.log('object')
+// }
+import FileSaver from 'file-saver';
+import { surpriseMePrompts } from '../constants';
+
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+}
+
+export async function downloadImage(_id, photo) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
